0b3b8b28a98905181285dac293a15f36054be4da
KrissurK <119725996+KrissurK@users.noreply.github.com>
2023-04-05 11:06:05 +0300

    uuendatud: hero image kõrgus, piltide paddingud, continue shopping nupp

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 document.querySelectorAll('button[href]').forEach((item) => {
     item.addEventListener('click', (event) => {
+        console.log(event.target);
         location.href=event.target.getAttribute('href');
     });
 });
